ac1d4f5eb63fae12b85ab1cde4787beca3970aa5
baderr66 <143142004+baderr66@users.noreply.github.com>
2023-08-26 23:31:05 +0300

    done

---
+++ main.py
@@ -0,0 +1,40 @@
+my_name = input("enter your name: ")
+my_age = input("enter your age: ")
+
+print("my name is {my name} and I am {my age} years oid")
+
+
+
+first_number =float (input("number one"))
+second_number = float(input("number tow: "))
+operation = input("enter an operation")
+
+if operation =="+":
+    print(first_number + second_number)
+elif operation== '-'
+    print(first_number - second_number)
+elif operation== '*'
+    print(first_number * second_number)
+elif operation== '/'
+    print(first_number / second_number)
+
+else:
+print("the operation is not valid")
+
+
+
+
+bus_capacity = 27
+in_bus = int(input("how many person in the bus"))
+
+join_bus = int(input("how many people in the bus"))
+
+empty_seats = bus_capacity - in_bus 
+ print(empty_seats)
+
+if empty_seats > bus_capacity :
+   print(f"there are empty seats . the empty seats are {empty}")
+
+
+else: 
+   print("sorry there are no empty seats ")
